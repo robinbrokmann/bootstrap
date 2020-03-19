3e66e6351b3876c79d32238658650567fbf72a9e
robin <106778@student.drenthecollege.nl>
2020-03-19 10:11:48 +0100

    cruise pagina af

--- cruise.php
+++ cruise.php
@@ -11,5 +11,24 @@ require_once "nav.php";
     <div class="header-div">
         <img alt="header" class="img-fluid w-100 header-img" src="img/header5.jpg">
     </div>
+    <div class="container-fluid text2  w-100">
+        <div class="row">
+            <div class="col-sm-12 ">
+                <br>
+                <h2>Sabaton Cruise</h2>
+                <br>
+                <p>De Sabaton cruise is een jaarlijks evenement in December vergelijkbaar met 7000 tons of metal een van de grootste metal cruises van de wereld.<br>
+                    Sabaton organiseerd dit zelf, De cruise duurt 53 uur in die tijd heb je meerder optredens van verschillende bands.<br>
+                    Vanuit Stockholm vertrek je op The Baltic Queen Tallinn waar je ook een dag vrij rond kan lopen.<br>
+                    voor dit jaar zijn nog geen andere bands dan Sabaton zelf bekend gemaakt.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div  class="cruise_img"> 
+        <img src="img/cruise.jpg" class="img-fluid">
+    </div>
+
 <?php
 require_once "footer.php";
